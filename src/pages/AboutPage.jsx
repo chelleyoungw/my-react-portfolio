a4b56497f0c706be7076b5dab9cf337030896f98
Chelle Wood <chelleyoungw@gmail.com>
2024-06-24 18:34:07 -0500

    added styling to about page, profile pic, and css styling

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import portraitImg from '../assets/img/profile-pic.png';
+
+export default function AboutPage() {
+  return (
+    <div className='about-page'>
+      <div className='about-content-container'>
+        <h1>About Me</h1>
+        <p>INSERT MY BIO HERE</p>
+        <img src={portraitImg} alt='Chelle Wood smiling' />
+        <p>
+          ADD IN MY DETAILED BIO
+        </p>
+      </div>
+    </div>
+  );
+}
