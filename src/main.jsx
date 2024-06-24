203ab8df5f4a3e45b176971a28ea7e7d110b77e8
Chelle Wood <chelleyoungw@gmail.com>
2024-06-24 18:22:27 -0500

    added routes for different pages in main.jsx and added layout for main page app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,53 @@
+// Imports required imports from React.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+// Imports required imports from 'react-router-dom' to set up application routing behavior.
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Imports required imports from Bootstrap to apply CSS.
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
+// Imports the App.jsx file which works with the router.
+import App from './App.jsx'
+
+// Imports pages the router will use to conditionally show the appropriate views.
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
+
+// Defines the accessible routes, and which components to respond to which URL.
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: '/profile',
+        element: <ProfilePage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+// Renders the RouterProvider component to the HTML.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
