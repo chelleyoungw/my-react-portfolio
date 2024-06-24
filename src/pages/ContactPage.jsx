b4be4599160d8ec78af68a68f6086d05072e13bf
Chelle Wood <chelleyoungw@gmail.com>
2024-06-24 18:50:34 -0500

    added formatting and information to contactpage

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function ContactPage() {
+    return(
+        <div class = "column-container">
+            <div>
+                <h1>Contact Me!</h1>
+            </div>
+            <div class = "card">
+                <h2>chelleyoungw@gmail.com</h2>
+            </div>
+            <div class = "card">
+                <h2>713-380-8887</h2>
+            </div>
+            <div class = "card">
+                <a class = "linkedin" href = "https://linkedin.com/in/chelle-wood-bs-cvai-b1231511a">Chelle Wood</a>
+            </div>
+        </div>
+    ) 
+}
