0d03ad4832a76cdabd6fc94399bb5f0dc1efe2b9
Chelle Wood <chelleyoungw@gmail.com>
2024-06-24 18:25:48 -0500

    added formatting and logo links for footer for github and linked in

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import githubImg from '../assets/img/logo_github.svg';
+import linkedinImg from '../assets/img/logo_linkedin.svg';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='footerContents'>
+        <a href='https://github.com/chelleyoungw' target='_blank'>
+          <img src={githubImg} alt='GitHub Logo' />
+        </a>
+        <a href='https://linkedin.com/in/chelle-wood-bs-cvai-b1231511a' target='_blank'>
+          <img src={linkedinImg} alt='LinkedIn Logo' />
+        </a>
+      </div>
+    </footer>
+  );
+}
